7a3ab654dc11c87127e7ecd12b999977fef2e2eb
skotsiev <skotsiev@gmail.com>
2022-07-22 20:05:30 +0300

    add delimeter option, gitignore

--- src/main/java/StreamExecution.java
+++ src/main/java/StreamExecution.java
@@ -14,27 +14,29 @@ public class StreamExecution {
 
         StructType schemaLineitem = new StructType()
                 .add("L_ORDERKEY", DataTypes.IntegerType)
-                .add("l_partkey", DataTypes.IntegerType)
-                .add("l_suppkey", DataTypes.IntegerType)
-                .add("l_linenumber", DataTypes.IntegerType)
-                .add("l_quantity", DataTypes.IntegerType)
-                .add("l_extendedprice", DataTypes.DoubleType)
-                .add("l_discount", DataTypes.DoubleType)
-                .add("l_tax", DataTypes.DoubleType)
-                .add("l_returnflag", DataTypes.StringType)
-                .add("l_linestatus", DataTypes.StringType)
-                .add("l_shipdate", DataTypes.StringType)
-                .add("l_commitdate", DataTypes.StringType)
-                .add("l_receiptdate", DataTypes.StringType)
-                .add("l_shipinstruct", DataTypes.StringType)
-                .add("l_shipmode", DataTypes.StringType)
-                .add("l_comment", DataTypes.StringType);
+                .add("L_PARTKEY", DataTypes.IntegerType)
+                .add("L_SUPPKEY", DataTypes.IntegerType)
+                .add("L_LINENUMBER", DataTypes.IntegerType)
+                .add("L_QUANTITY", DataTypes.IntegerType)
+                .add("L_EXTENDEDPRICE", DataTypes.DoubleType)
+                .add("L_DISCOUNT", DataTypes.DoubleType)
+                .add("L_TAX", DataTypes.DoubleType)
+                .add("L_RETURNFLAG", DataTypes.StringType)
+                .add("L_LINESTATUS", DataTypes.StringType)
+                .add("L_SHIPDATE", DataTypes.StringType)
+                .add("L_COMMITDATE", DataTypes.StringType)
+                .add("L_RECEIPTDATE", DataTypes.StringType)
+                .add("L_SHIPINSTRUCT", DataTypes.StringType)
+                .add("L_SHIPMODE", DataTypes.StringType)
+                .add("L_COMMENT", DataTypes.StringType);
 
         final String lineitemFile = "/home/soslan/Desktop/data/0.1GB/lineitem*.csv";
 
         Dataset<Row> lineItemStream = spark.readStream()
                 .option("header", false)
+                .option("delimiter","|")
                 .format("csv")
+
                 .schema(schemaLineitem)
                 .csv(lineitemFile);
 
