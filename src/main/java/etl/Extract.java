3033de11142c8764b91d80bcf3d0bdb2e5cef5e9
skotsiev <skotsiev@gmail.com>
2022-08-18 18:38:32 +0300

    pipelines with delta forgotten file

--- src/main/java/etl/Extract.java
+++ src/main/java/etl/Extract.java
@@ -46,4 +46,27 @@ public class Extract {
         }
             return dataFrame;
     }
+
+    public Dataset<Row> extractFromDelta(){
+
+
+        final String rootPath = "/tmp/delta-";
+        final String path = rootPath + name;
+
+        System.out.println("[" + getClass().getSimpleName() + "]\t\t" + "Import data from delta-" + name + ".csv");
+        System.out.println("[" + getClass().getSimpleName() + "]\t\t" + "Start reading data");
+        long start = System.currentTimeMillis();
+        Dataset<Row> df = spark.read().format("delta")
+                .load(path);
+        long end = System.currentTimeMillis();
+        long elapsedTime = end - start;
+        if (elapsedTime < 1000){
+            System.out.println("[" + getClass().getSimpleName() + "]\t\t" + "Elapsed time to read" + df.count() + " lines: " + elapsedTime + " millis");
+        }
+        else {
+            long elapsedTimeSeconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime);
+            System.out.println("[" + getClass().getSimpleName() + "]\t\t" + "Elapsed time to read: " + df.count() + " lines: " + elapsedTimeSeconds + " seconds");
+        }
+        return df;
+    }
 }
